f4bea670af21c78e807499fffa7b62fa022c2863
RCMAN <franco.gaetan@gmail.com>
2024-09-11 08:40:48 -0400

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,1 +1,20 @@
-sudo apt-get install git fakeroot build-essential ncurses-dev xz-utils libssl-dev bc flex libelf-dev bison mc tree vim curl python3 cmake scons neofetch openssh-server minicom -y
+sudo apt install git -y
+sudo apt fakeroot 
+sudo apt build-essential 
+sudo apt ncurses-dev 
+sudo apt xz-utils 
+sudo apt libssl-dev 
+sudo apt bc 
+sudo apt flex 
+sudo apt libelf-dev 
+sudo apt bison 
+sudo apt mc 
+sudo apt tree 
+sudo apt vim 
+sudo apt curl 
+sudo apt python3 
+sudo apt cmake 
+sudo apt scons 
+sudo apt neofetch 
+sudo apt openssh-server 
+sudo apt minicom -y
