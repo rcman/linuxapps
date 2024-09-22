3b7dcd62b0e8713217ecd29149ae036e3c8dc11b
RCMAN <franco.gaetan@gmail.com>
2024-09-22 17:56:40 -0400

    Update compile2.sh

--- compile2.sh
+++ compile2.sh
@@ -5,4 +5,13 @@
 
  btrfsmaintenance
 
+
+sudo apt install libncurses-dev gawk flex bison openssl libssl-dev dkms libelf-dev libudev-dev libpci-dev libiberty-dev autoconf llvm
+
+ sudo apt-get install bc bison build-essential coccinelle   device-tree-compiler dfu-util efitools flex gdisk graphviz imagemagick   liblz4-tool libgnutls28-dev libguestfs-tools libncurses-dev   libpython3-dev libsdl2-dev libssl-dev lz4 lzma lzma-alone openssl   pkg-config python3 python3-asteval python3-coverage python3-filelock   python3-pkg-resources python3-pycryptodome python3-pyelftools   python3-pytest python3-pytest-xdist python3-sphinxcontrib.apidoc   python3-sphinx-rtd-theme python3-subunit python3-testtools   python3-virtualenv swig uuid-dev
+
+ sudo apt install ecdsautils btrfsmaintenance  -y
+
+ btrfsmaintenance libcrcutil-dev
+ 
  
